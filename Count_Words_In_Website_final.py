c5e83f4fa43905d60cf5e7791f6750d744e8227d
BL4CKF0Xk <kavinduksks@gmail.com>
2024-07-22 21:59:44 +0530

    Final Project Started

--- Count_Words_In_Website_final.py
+++ Count_Words_In_Website_final.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import click
 
 def get_html_of(url):
     resp = requests.get(url)
@@ -11,10 +12,12 @@ def get_html_of(url):
 
     return resp.content.decode()
 
-def count_occ_in(word_list):
+def count_occ_in(word_list,min_lenght):
     word_count = {}
 
     for word in word_list:
+        if len(word) < min_lenght:
+            continue
         if word not in word_count:
             word_count[word] = 1
 
@@ -29,14 +32,29 @@ def get_all_words(url):
     raw_text = soup.get_text()
     return re.findall(r'\w+', raw_text)
 
-def get_top_words(all_words):
-    occr = count_occ_in(all_words)
+def get_top_words(all_words,min_lenght):
+    occr = count_occ_in(all_words, min_lenght)
     return sorted(occr.items(), key=lambda item: item[1], reverse=True)
 
-if __name__ == '__main__':
+def write_to_file(output, top_words):
+    f = open(f"{output}.txt", "w")
+    for i in range(10):
+        f.write(top_words[i][0])
+    f.close()
+
+@click.command()
+@click.option('--url', '-u', prompt='Web Url', help='URL of the webpage')
+@click.option('--lenght', '-l', default=0, help='Minimum word lenght')
+@click.option('--output', '-u', prompt='File Name', help='File name to be saved') 
+
+def main(url, lenght, output):
     page_url = 'https://academy.hackthebox.com/module/88/section/924'
-    the_words = get_all_words(page_url)
-    top_words = get_top_words(the_words)
+    the_words = get_all_words(url)
+    top_words = get_top_words(the_words, lenght)
+    write_to_file(output, top_words)
 
     for i in range(10):
         print(top_words[i][0])
+
+if __name__ == '__main__':
+    main()
